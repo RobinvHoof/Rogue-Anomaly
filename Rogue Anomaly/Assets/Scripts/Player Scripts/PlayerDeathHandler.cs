a676a7de8f54cf723b3607be2bd0520380f83a2c
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-09-15 15:18:09 +0200

    DeathHandler now freezes game and loads canvas

--- Rogue Anomaly/Assets/Scripts/Player Scripts/PlayerDeathHandler.cs
+++ Rogue Anomaly/Assets/Scripts/Player Scripts/PlayerDeathHandler.cs
@@ -1,16 +1,22 @@
-using System;
 using UnityEngine;
+using StandardAssets;
 
 public class PlayerDeathHandler : MonoBehaviour
 {
-    void Start()
-    {
-        
+    [SerializeField] Canvas gameOverCanvas;
+    FPSController playerController;
+
+    private void Start() {
+        gameOverCanvas.enabled = false;
+        playerController = GetComponent<FPSController>();
     }
 
     public void HandleDeath()
     {
-        // TODO something happens when you die
-        throw new NotImplementedException();
+        playerController.enabled = false;
+        gameOverCanvas.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
