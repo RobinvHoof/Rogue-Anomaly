a91a7e157e6eceb5a8948077373f422efafe360b
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-09-15 22:02:13 +0200

    Player regenerate health over time

--- Rogue Anomaly/Assets/Scripts/Player/PlayerHealth.cs
+++ Rogue Anomaly/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,16 +3,38 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int maxHealth = 100;
+    [SerializeField] float healthRegenerationRate = 1;
+    [SerializeField] float regenerationDelay = 1;
     int hitPoints;
+    float regenBuffer;
+    float lastHitTime = -1;
 
     void Start()
     {
         hitPoints = maxHealth;
     }
 
+    void Update()
+    {
+        if (lastHitTime >= 0 && Time.time - lastHitTime >= regenerationDelay)
+        {
+            RegenerateHealth();
+        }
+    }
+
+    void RegenerateHealth()
+    {
+        regenBuffer += healthRegenerationRate * Time.deltaTime;
+        int regenAmount = Mathf.FloorToInt(regenBuffer);
+        regenBuffer -= regenAmount;
+        if (regenAmount > 0) RestoreHealth(regenAmount);
+    }
+
     public void TakeDamage(int damageAmount)
     {
         hitPoints -= damageAmount;
+        lastHitTime = Time.time;
+        regenBuffer = 0;
 
         if (hitPoints <= 0)
         {
@@ -23,6 +45,12 @@ public class PlayerHealth : MonoBehaviour
     public void RestoreHealth(int healthAmount)
     {
         hitPoints = Mathf.Clamp(hitPoints + healthAmount, 0, maxHealth);
+
+        if(hitPoints == maxHealth)
+         {
+            lastHitTime = -1;
+            regenBuffer = 0;
+         }
     }
 
     public int GetCurrentHitPoints()
