4bc037d16e7a94fcdbe1371aeea89e4af61fa95b
Nathan Thus <nathan.thus@outlook.com>
2022-09-16 16:23:08 +0200

    Update EnemyWeapon.cs
    Configurable bulletspeed, as well as proper implementation for fire rate.

--- Rogue Anomaly/Assets/Scripts/Enemy/EnemyWeapon.cs
+++ Rogue Anomaly/Assets/Scripts/Enemy/EnemyWeapon.cs
@@ -6,20 +6,19 @@ using UnityEngine;
 public class EnemyWeapon : MonoBehaviour
 {
     [SerializeField]
-    GameObject WeaponProjectile;
-
-    [SerializeField]
-    GameObject ImpactEffect;
-
-    [SerializeField]
     [Range(0, 2)]
     float fireDelay = 2;
 
     [SerializeField]
     GameObject bulletPrefab;
 
+    [SerializeField]
+    [Range(0, 2)]
+    float bulletSpeed;
+
     Transform target;
 
+    bool readyToShoot = true;
 
     private void Start()
     {
@@ -28,13 +27,19 @@ public class EnemyWeapon : MonoBehaviour
 
     private void Update()
     {
-        FireWeapon();
+        if (readyToShoot)
+        { 
+            StartCoroutine(FireWeapon());
+        }
     }
 
     private IEnumerator FireWeapon()
     {
-        GameObject instance =Instantiate(bulletPrefab,transform.position,transform.rotation);
-        instance.GetComponent<Rigidbody>().AddForce(target.position * 2, ForceMode.Impulse);
+        readyToShoot = false;
+        GameObject instance = Instantiate(bulletPrefab,transform.position,transform.rotation);
+        instance.GetComponent<Rigidbody>().AddForce((target.position - transform.position).normalized * bulletSpeed, ForceMode.Impulse);
+        Destroy(instance, 3f);
         yield return new WaitForSeconds(fireDelay);
+        readyToShoot = true;
     }
 }
