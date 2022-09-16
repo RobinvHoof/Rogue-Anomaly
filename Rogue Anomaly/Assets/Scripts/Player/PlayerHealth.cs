f14335c7bccbe5dc67f597ffe1393d30c23eeafd
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-09-16 10:26:55 +0200

    IAttackable interface added

--- Rogue Anomaly/Assets/Scripts/Player/PlayerHealth.cs
+++ Rogue Anomaly/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : MonoBehaviour, IAttackable
 {
     [SerializeField] int maxHealth = 100;
     [SerializeField] float healthRegenerationRate = 1;
