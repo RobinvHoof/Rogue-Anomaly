3be093bfa400df7309ccd7dac3eba5afdae6f292
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-09-19 20:24:31 +0200

    AmmoPickup animation

--- Rogue Anomaly/Assets/Scripts/Ammo/AmmoPickup.cs
+++ Rogue Anomaly/Assets/Scripts/Ammo/AmmoPickup.cs
@@ -5,11 +5,6 @@ public class AmmoPickup : MonoBehaviour
     [SerializeField] int increasePercentage = 25;
     [SerializeField] AmmoType ammoType;
 
-    void Update()
-    {
-        transform.Rotate(new Vector3(0, 1, 0));
-    }
-
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player")
         {
