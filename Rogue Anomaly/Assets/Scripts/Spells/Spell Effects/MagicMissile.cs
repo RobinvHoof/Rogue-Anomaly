768f0af8ed46e510b121fa9b3a1e0adcfb082a46
Nathan Thus <nathan.thus@outlook.com>
2022-09-20 12:56:42 +0200

    Created Magic Missile

---
+++ Rogue Anomaly/Assets/Scripts/Spells/Spell Effects/MagicMissile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicMissile : BaseSpellEffect
+{
+    public override void TriggerSpellEffect(Collider other)
+    {
+        
+    }
+}
