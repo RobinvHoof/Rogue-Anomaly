4cacb6b09353b7593af56e6ca60d409c7260bd0e
Nathan Thus <nathan.thus@outlook.com>
2022-09-20 15:23:50 +0200

    Created New SpellTrigger

---
+++ Rogue Anomaly/Assets/Scripts/Spells/SpellTriggerDrag.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpellTriggerDrag : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject impactParticle;
+    public List<GameObject> TaggedEntities;
+
+    public bool isMoving { get; private set; }
+
+    private void Start()
+    {
+        isMoving = true;
+        TaggedEntities = new List<GameObject>();
+        Instantiate(impactParticle, transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            TaggedEntities.Add(other.gameObject);
+        }
+        else if(other.tag == "Player") { };
+        this.GetComponent<BaseSpellEffect>().TriggerSpellEffect(other);
+    }
+
+    private void Update()
+    { 
+        if (gameObject.GetComponent<Rigidbody>().velocity.magnitude <= 1 && isMoving)
+        {
+            isMoving = false;
+        }
+    }
+}
