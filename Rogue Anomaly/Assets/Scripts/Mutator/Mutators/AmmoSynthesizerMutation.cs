eade501a11294f3788825e1172efd7d5f2e0c5a4
428780 <robbie@vanhoof-erp.nl>
2022-09-21 10:41:49 +0200

    Ammo Synthesizer Mutation Implementation

---
+++ Rogue Anomaly/Assets/Scripts/Mutator/Mutators/AmmoSynthesizerMutation.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoSynthesizerMutation : BaseMutation
+{
+    [SerializeField]
+    public AmmoManager ammoManager;
+
+    [SerializeField]
+    [Range(0, 1)]
+    public float maxAmmoMultiplier = 0.75f;
+
+    [SerializeField]
+    [Range(0, 100)]
+    public int regenAmountPerTick = 5;
+
+    public AmmoSynthesizerMutation() : base("Doubletap", "If one shot isnt enough make sure to doubletap to secure the kill! Your accuracy is slightly reduced but you shoot an extra pallet per shot for free!") {}
+
+    private Dictionary<AmmoType, int> subtractedAmmoCap;
+
+    public void Start() 
+    {
+        subtractedAmmoCap = new();
+    }
+
+    public override void TriggerEvent(GameObject source, string eventName, params object[] parameters)
+    {
+        switch(eventName)
+        {           
+            case "startMutation":
+                foreach(AmmoManager.AmmoSlot slot in ammoManager.ammoSlots)
+                {
+                    subtractedAmmoCap.Add(slot.ammoType, (int)(slot.maxAmmo * (1 - maxAmmoMultiplier)));
+                    slot.maxAmmo = (int)((float)slot.maxAmmo * maxAmmoMultiplier);
+                }
+                
+                break;
+            
+            case "stopMutation":
+                foreach(AmmoManager.AmmoSlot slot in ammoManager.ammoSlots)
+                {
+                    slot.maxAmmo += subtractedAmmoCap[slot.ammoType];
+                }
+                break;
+
+            case "timerTick":
+                foreach(AmmoManager.AmmoSlot slot in ammoManager.ammoSlots)
+                {
+                    ammoManager.IncreaseAmmo(slot.ammoType, regenAmountPerTick);
+                }
+                break;
+        }
+    }
+}
